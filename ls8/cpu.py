c9672f40778ad142c79ce2ceda782c5b25ed03d0
janecyyu <janecyyu@gmail.com>
2020-07-22 11:03:06 -0700

    stack ok

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,6 +7,9 @@ LDI = 0b10000010  # LDI R0,8
 PRN = 0b01000111  # PRN R0
 HLT = 0b00000001
 MUL = 0b10100010  # MUL R0,R1
+PUSH = 0b01000101  # PUSH R0
+POP = 0b01000110  # POP R2
+SP = 7
 
 
 class CPU:
@@ -17,11 +20,14 @@ class CPU:
         self.reg = [0] * 8
         self.ram = [0] * 256
         self.pc = 0
+        self.sp = 7
         self.branchtable = {}
         self.branchtable[HLT] = self.hlt
         self.branchtable[LDI] = self.ldi
         self.branchtable[PRN] = self.prn
         self.branchtable[MUL] = self.mul
+        self.branchtable[PUSH] = self.push
+        self.branchtable[POP] = self.pop
 
     def ram_read(self, address):
         return self.ram[address]
@@ -55,20 +61,6 @@ class CPU:
             print(f"Couldn't find file {sys.argv[1]}")
             sys.exit(1)
 
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
@@ -100,9 +92,32 @@ class CPU:
 
         print()
 
+    def push(self):
+        self.reg[SP] -= 1
+
+        reg_num = self.ram_read(self.pc + 1)
+        value = self.reg[reg_num]
+        # print(self.reg[SP])
+        push_to = self.reg[SP]
+        print(push_to)
+        self.ram[push_to] = value
+
+        self.pc += 2
+
+    def pop(self):
+        address_pop_from = self.reg[SP]
+        value = self.ram[address_pop_from]
+
+        reg_num = self.ram[self.pc + 1]
+        self.reg[reg_num] = value
+
+        self.reg[SP] += 1
+
+        self.pc += 2
+
     def mul(self):
-        operand_a = self.ram[self.pc + 1]
-        operand_b = self.ram[self.pc + 2]
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
         self.alu("MUL", operand_a, operand_b)
         self.pc += 3
 
